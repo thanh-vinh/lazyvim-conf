17fa391acbf7d28a36cf2753613c83c342682dbf
Vinh <thanh.vinh@hotmail.com>
2024-10-15 15:46:11 +0700

    cmd: disable autoformat

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -6,3 +6,12 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+-- https://www.lazyvim.org/configuration/tips#disable-autoformat-for-some-buffers
+-- Disable autoformat for `py` files
+vim.api.nvim_create_autocmd({ "FileType" }, {
+  pattern = { "py" },
+  callback = function()
+    vim.b.autoformat = false
+  end,
+})
